e3ff98b5205e45a949a2b101862465201d680812
kats205 <npbaokhanhpro@gmail.com>
2026-05-19 10:07:34 +0700

    chore(scaffold): initialize flutter project with platform configurations

---
+++ lab1/test/widget_test.dart
@@ -0,0 +1,21 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import 'package:lab1/apps/jobspot_app.dart';
+
+void main() {
+  testWidgets('Jobspot app opens on splash screen', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(const JobspotApp());
+
+    expect(find.byType(SvgPicture), findsOneWidget);
+  });
+}
